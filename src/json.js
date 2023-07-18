a47e8c8259829de236ddd5fc00f1769b5c93114c
rabnoor <56947486+rabnoor@users.noreply.github.com>
2023-07-18 14:25:51 -0600

    Initial commit

---
+++ src/json.js
@@ -0,0 +1,27 @@
+export const json = {
+  "elements": [{
+    "type": "boolean",
+    "name": "slider",
+    "title": "Are you 21 or older?",
+    "description": "Display mode = Default (Slider)",
+    "valueTrue": "Yes",
+    "valueFalse": "No"
+  }, {
+    "type": "boolean",
+    "name": "radiobutton",
+    "title": "Are you 21 or older?",
+    "description": "Display mode = Radio",
+    "valueTrue": "Yes",
+    "valueFalse": "No",
+    "renderAs": "radio"
+  }, {
+    "type": "boolean",
+    "name": "checkbox",
+    "label": "I am 21 or older",
+    "titleLocation": "hidden",
+    "valueTrue": "Yes",
+    "valueFalse": "No",
+    "renderAs": "checkbox"
+  }],
+  "showQuestionNumbers": false
+};
